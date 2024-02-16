6eeb4bb8df605f31d90bdb110324485bb9d69787
philschmid <schmidphilipp1995@gmail.com>
2024-02-16 17:00:23 +0000

    wip

---
+++ training/dpo-trl.py
@@ -0,0 +1,95 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from datasets import load_dataset
+from trl import DPOTrainer
+from peft import LoraConfig
+
+# Load jsonl data from disk
+train_dataset = load_dataset("json", data_files="train_dataset.json", split="train")
+eval_dataset = load_dataset("json", data_files="test_dataset.json", split="train")
+
+# Hugging Face model id
+model_id = "cognitivecomputations/dolphin-2.1-mistral-7b" # replace with your model id
+
+# BitsAndBytesConfig int-4 config
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+# )
+
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    use_cache=False, 
+    attn_implementation="flash_attention_2",
+    torch_dtype=torch.bfloat16,
+    # quantization_config=bnb_config
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+
+prompt_length=1024
+max_seq_length=1512
+
+
+# LoRA config based on QLoRA paper & Sebastian Raschka experiment
+peft_config = LoraConfig(
+        lora_alpha=32,
+        lora_dropout=0.05,
+        r=16,
+        bias="none",
+        target_modules="all-linear",
+        task_type="CAUSAL_LM", 
+)
+
+from transformers import TrainingArguments
+
+args = TrainingArguments(
+    overwrite_output_dir=True,              # overwrite the content of the output directory
+    output_dir="doplhin-dpo",               # directory to save and repository id
+    num_train_epochs=2,                     # number of training epochs
+    per_device_train_batch_size=8,         # batch size per device during training
+    per_device_eval_batch_size=4,           # batch size for evaluation
+    gradient_accumulation_steps=1,          # number of steps before performing a backward/update pass
+    gradient_checkpointing=True,            # use gradient checkpointing to save memory
+    gradient_checkpointing_kwargs={'use_reentrant':False}, 
+    optim="adamw_torch_fused",              # use fused adamw optimizer
+    # learning_rate=2e-4,                     # learning rate, based on QLoRA paper
+    max_grad_norm=0.3,                      # max gradient norm based on QLoRA paper
+    warmup_ratio=0.1,                       # warmup ratio based on QLoRA paper
+    lr_scheduler_type="cosine",             # use cosine learning rate scheduler
+    logging_steps=25,                       # log every 25 steps
+    save_steps=500,                         # when to save checkpoint
+    save_total_limit=2,                     # limit the total amount of checkpoints
+    evaluation_strategy="steps",            # evaluate every 1000 steps
+    eval_steps=700,                         # when to evaluate
+    bf16=True,                              # use bfloat16 precision
+    tf32=True,                              # use tf32 precision
+    push_to_hub=False,                      # push model to hub
+    report_to="tensorboard",                # report metrics to tensorboard
+)
+
+dpo_args = {
+    "beta": 0.1,                           # The beta factor in DPO loss. Higher beta means less divergence
+    "loss_type": "sigmoid"                  # The loss type for DPO.
+}
+
+trainer = DPOTrainer(
+    model,
+    ref_model=None, # set to none since we use peft
+    peft_config=peft_config,
+    args=args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    tokenizer=tokenizer,
+    max_length=max_seq_length,
+    max_prompt_length=prompt_length,
+    beta=dpo_args["beta"],
+    loss_type=dpo_args["loss_type"],
+)
+
+# start training, the model will be automatically saved to the hub and the output directory
+trainer.train()
+
+# save model at the end of training
+trainer.save_model()
